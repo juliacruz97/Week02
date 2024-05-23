7a68425451b55b3b5395fa937121a2956968fbd7
juliacruz97 <juliacruz97@hotmail.com>
2024-05-23 11:03:30 -0300

    23_05_T11:03am

---
+++ addOne.py
@@ -0,0 +1,8 @@
+# addOne00.py
+# This program permit you includ a number 
+# that will be reading on a number and prints out one more than that number
+# author: Julia Cruz
+
+number = int(input('Please enter a number:' ))
+newnumber = number + 1
+print ( f'{number} plus one is {newnumber}')
