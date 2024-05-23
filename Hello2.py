fc6b2e6e2041e8a865255baf62c8e887ca9218c1
juliacruz97 <juliacruz97@hotmail.com>
2024-05-23 10:51:05 -0300

    23_05 T10:50am

---
+++ Hello2.py
@@ -0,0 +1,10 @@
+# hello2.py
+# This program just prints out hello
+# author: Julia Cruz
+# If we didnt ('Hello ') was not inside the
+#If we didn't include the space between 'Hello' and the name, when printing the message it would show 
+# everything together without the space
+
+name= input("Enter your name: ")
+#print('Hello ' + name 'n\Nice to meet you')
+print(f'Hello {name}\nNice to meet you')
